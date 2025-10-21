744fe8125477ab0c853db8e15258a59f38cb6846
AnatoliyPolyudov <49663495+AnatoliyPolyudov@users.noreply.github.com>
2025-10-21 15:34:31 +0300

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -1,5 +1,9 @@
+import asyncio
 from telegram import Bot
 
-bot = Bot(token="8436652130:AAF6On0GJtRHfMZyqD3mpM57eXZfWofJeng")
-print(bot.get_me())
+async def main():
+    bot = Bot(token="8436652130:AAF6On0GJtRHfMZyqD3mpM57eXZfWofJeng")
+    me = await bot.get_me()
+    print(me)
 
+asyncio.run(main())
