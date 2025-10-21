fb69fbdc375b7a6fcc41e933d0b93132c10505ca
AnatoliyPolyudov <49663495+AnatoliyPolyudov@users.noreply.github.com>
2025-10-21 08:26:12 +0300

    Update observer.py

--- observer.py
+++ observer.py
@@ -1,17 +1,22 @@
-# observer.py
-class Observer: """Observer interface.""" def 
-    update(self, data):
-        raise NotImplementedError("Observer subclasses 
-        must implement 'update' method.")
-class Subject: """Base class for observable objects.""" 
+class Observer:
+    """Observer interface."""
+    def update(self, data):
+        raise NotImplementedError("Observer subclasses must implement 'update' method.")
+
+
+class Subject:
+    """Base class for observable objects."""
     def __init__(self):
-        self._observers = [] def attach(self, observer: 
-    Observer):
-        if observer not in self._observers: 
+        self._observers = []
+
+    def attach(self, observer: Observer):
+        if observer not in self._observers:
             self._observers.append(observer)
-    def detach(self, observer: Observer): if observer 
-        in self._observers:
-            self._observers.remove(observer) def 
-    notify(self, data=None):
-        for observer in self._observers: 
+
+    def detach(self, observer: Observer):
+        if observer in self._observers:
+            self._observers.remove(observer)
+
+    def notify(self, data=None):
+        for observer in self._observers:
             observer.update(data)
