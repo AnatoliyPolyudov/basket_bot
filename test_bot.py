22bf7cc3390e7ea1fb021516a920be2caa862c5b
Your Name <you@example.com>
2025-10-22 15:23:49 +0300

    Update: 2025-10-22 15:23:49

--- test_bot.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from telegram import Bot
-
-async def main():
-    bot = Bot(token="8436652130:AAF6On0GJtRHfMZyqD3mpM57eXZfWofJeng")
-    me = await bot.get_me()
-    print(me)
-
-asyncio.run(main())
--- test_send.py
+++
@@ -1,8 +0,0 @@
-from telegram import Bot
-import asyncio
-
-async def main():
-    bot = Bot(token="8436652130:AAF6On0GJtRHfMZyqD3mpM57eXZfWofJeng")
-    await bot.send_message(chat_id="317217451", text="✅ Test message from script")
-
-asyncio.run(main())
