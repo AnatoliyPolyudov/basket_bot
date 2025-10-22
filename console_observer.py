f12788c58a3a69effd4b0de10b3fc9376ab02229
AnatoliyPolyudov <49663495+AnatoliyPolyudov@users.noreply.github.com>
2025-10-22 14:36:35 +0300

    Update console_observer.py

--- console_observer.py
+++ console_observer.py
@@ -5,3 +5,4 @@ class ConsoleObserver(Observer):
     def update(self, data):
         print(f"[{datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')}] Observer received update:")
         print(f"  Signal: {data['signal']}, Z-score: {data['z']:.4f}, Spread: {data['spread']:.6f}")
+
